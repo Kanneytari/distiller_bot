8c9030c484478d9db6ca10787c738138578af370
Kanneytari <hitroplan@yandex.ru>
2026-08-28 21:25:30 +0300

    Remove temporary callback helper

--- distiller_bot/keyboards_second_distillation_patch.py
+++
@@ -1,6 +0,0 @@
-SECOND_DISTILLATION_PROCESS_ACTION_CALLBACKS = {
-    "second_distillation": "process:second-distillation:{process_id}",
-    "second_distillation_calculators": "process:second-distillation-calculators:{process_id}",
-}
-
-SECOND_DISTILLATION_SECONDARY_ACTIONS = {"second_distillation_calculators"}
