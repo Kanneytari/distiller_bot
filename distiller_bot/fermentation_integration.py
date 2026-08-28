ae026fc071579505f3a24641c5b20a6e0559a2ec
Kanneytari <hitroplan@yandex.ru>
2026-08-28 21:48:36 +0300

    Keep fermentation measurement UI minimal

--- distiller_bot/fermentation_integration.py
+++ distiller_bot/fermentation_integration.py
@@ -1,4 +1,5 @@
 from .keyboards import PROCESS_ACTION_CALLBACKS
+from .processes import STAGE_MEASUREMENT_ORDER, STAGE_QUICK_MEASUREMENTS
 
 
 PROCESS_ACTION_CALLBACKS.update(
@@ -7,3 +8,6 @@ PROCESS_ACTION_CALLBACKS.update(
         "fermentation_brix": "process:fermentation-brix:{process_id}",
     }
 )
+
+STAGE_MEASUREMENT_ORDER["fermentation"] = ["temperature"]
+STAGE_QUICK_MEASUREMENTS["fermentation"] = []
