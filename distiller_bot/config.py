557bfa715c5aa07a40f0e4ca7e53453920bd8b07
Kanneytari <hitroplan@yandex.ru>
2026-08-28 16:15:17 +0300

    refactor: default database to SQLite

--- distiller_bot/config.py
+++ distiller_bot/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     bot_token: str
-    database_url: str
+    database_url: str = "sqlite+aiosqlite:///distiller_bot.db"
 
     model_config = SettingsConfigDict(
         env_file=".env",
